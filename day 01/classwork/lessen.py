3f26918049774ee9b4dcb87a75948cb941162795
damigoa87 <damianekiknadze@gmail.com>
2026-02-25 18:40:08 +0400

    first commit

---
+++ day 01/classwork/lessen.py
@@ -0,0 +1,5 @@
+my_best_friend="kvicha"
+#ქეის სენსიტიურობა არის თუ დავწერთ A დიდით a პატარით კომოიუტერი მიხვდება
+#რომ ორივე ა ასოა და Aდა aსაც დაპრინტავს 
+my_favorite_sabgjegt="match"
+my_sister_name="nene"
